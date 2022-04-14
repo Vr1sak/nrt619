9695a63a1a84fd7d98797db249f8846f5981fa82
Vr1sak <nebojsavm@gmail.com>
2022-04-14 23:18:29 +0200

    Porukica

---
+++ nrt619.zadatak1.js
@@ -0,0 +1,12 @@
+import {
+  evaluate
+  } from 'mathjs'
+ 
+  
+  console.log(evaluate('12.7 cm to inch') )   // 5 inch
+  console.log(evaluate('sin(45 deg) ^ 2'))    // 0.5
+  console.log(evaluate('det([-1, 2; 3, 1])')) // -7
+  
+  /*-1 2
+    3 1 
+  */
